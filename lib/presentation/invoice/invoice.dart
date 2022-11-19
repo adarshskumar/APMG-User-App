673feb4e417ad8010a186e863a0360342cfeed29
unknown <jishnu2109@gmail.com>
2022-11-19 20:48:48 +0700

    BottomNavigationBar

--- lib/presentation/invoice/invoice.dart
+++ lib/presentation/invoice/invoice.dart
@@ -8,10 +8,12 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../core/constants.dart';
-ValueNotifier<int> indexNotifier=ValueNotifier(0);
+
+ValueNotifier<int> indexNotifier = ValueNotifier(0);
+
 class Invoice extends StatefulWidget {
-   Invoice({Key? key}) : super(key: key);
-  final pages=[
+  Invoice({Key? key}) : super(key: key);
+  final pages = [
     ScreenAccount(),
     ScreenItem(),
     ScreenBilling(),
@@ -23,74 +25,87 @@ class Invoice extends StatefulWidget {
 }
 
 class _InvoiceState extends State<Invoice> {
-  dynamic screennumber = 0;
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    return ValueListenableBuilder(valueListenable: indexNotifier, builder: (context, newIndex, _) {
-      return Scaffold(
-        backgroundColor: backgroundColor,
-        primary: true,
-        appBar: AppBar(
+    return ValueListenableBuilder(
+      valueListenable: indexNotifier,
+      builder: (context, newIndex, _) {
+        return Scaffold(
           backgroundColor: backgroundColor,
-          elevation: 0,
-          title: const Text(
-            "Invoice",
-            style: TextStyle(fontSize: 22, color: black),
+          primary: true,
+          appBar: AppBar(
+            backgroundColor: backgroundColor,
+            elevation: 0,
+            title: const Text(
+              "Invoice",
+              style: TextStyle(fontSize: 22, color: black),
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
-        ),
-        drawer: const Drawer(),
-        bottomNavigationBar: ConvexAppBar(
-          //initialActiveIndex: indexNotifier.value,
-          color: Colors.white,
-          backgroundColor: dark30,
-          height: height * 0.075,
-          activeColor: Colors.white.withOpacity(0.01),
-          items: const [
-            TabItem(
-                icon: Icon(
-                  FontAwesomeIcons.user,
-                  color: Colors.white,
-                ),
-                title: 'Account'),
-            TabItem(
-                icon: Icon(
-                  FontAwesomeIcons.plusCircle,
-                  color: Colors.white,
-                ),
-                title: 'Item'),
-            TabItem(
-                icon: Icon(
-                  FontAwesomeIcons.moneyBill,
-                  color: Colors.white,
-                ),
-                title: 'Billing'),
-            TabItem(
-                icon: Icon(
-                  FontAwesomeIcons.shippingFast,
-                  color: Colors.white,
-                ),
-                title: 'Shipping'),
-            TabItem(
-                icon: Icon(
-                  FontAwesomeIcons.receipt,
-                  color: Colors.white,
-                ),
-                title: 'Invoice'),
-          ],
-          onTap: (int index) {
-            indexNotifier.value=index;
-          },
-        ),
-        body: ValueListenableBuilder(
-          valueListenable: indexNotifier,
-          builder: (context, index, child) {
-            return widget.pages[index];
-          },
-        ),
-      );
-    },);
+          drawer: const Drawer(),
+          bottomNavigationBar: BottomNavigationBar(
+            //initialActiveIndex: indexNotifier.value,
+            selectedItemColor: Colors.white,
+            unselectedLabelStyle: const TextStyle(
+            decorationColor: Colors.white
+            ,color: Colors.white),
+            selectedLabelStyle: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w500),
+            //color: Colors.white,
+            currentIndex: indexNotifier.value,
+            //height: height * 0.075,
+            //activeColor: Colors.white.withOpacity(0.01),
+            items: const [
+              BottomNavigationBarItem(
+                  backgroundColor: dark70,
+                  icon: Icon(
+                    FontAwesomeIcons.user,
+                    color: Colors.white,
+                  ),
+                  label: 'Account'),
+              BottomNavigationBarItem(
+                  backgroundColor: dark70,
+                  icon: Icon(
+                    FontAwesomeIcons.plusCircle,
+                    color: Colors.white,
+                  ),
+                  label: 'Item'),
+              BottomNavigationBarItem(
+                  backgroundColor: dark70,
+                  icon: Icon(
+                    FontAwesomeIcons.moneyBill,
+                    color: Colors.white,
+                  ),
+                  label: 'Billing'),
+              BottomNavigationBarItem(
+                  backgroundColor: dark70,
+                  icon: Icon(
+                    FontAwesomeIcons.shippingFast,
+                    color: Colors.white,
+                  ),
+                  label: 'Shipping'),
+              BottomNavigationBarItem(
+                  backgroundColor: dark70,
+                  icon: Icon(
+                    FontAwesomeIcons.receipt,
+                    color: Colors.white,
+                  ),
+                  label: 'Invoice'),
+            ],
+            onTap: (int index) {
+              indexNotifier.value = index;
+            },
+          ),
+          body: ValueListenableBuilder(
+            valueListenable: indexNotifier,
+            builder: (context, index, child) {
+              return widget.pages[index];
+            },
+          ),
+        );
+      },
+    );
   }
 }
